18d10ae1ed91793540529a33157ba0678dc1ed7c
ZJSS <zjss@zjsdeMacBook-Pro.local>
2026-09-03 17:15:14 +0800

    启用compose相关配置，导入依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     buildFeatures {
         viewBinding = true
+        compose = true
     }
     namespace = "com.example.bmi"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -63,4 +65,14 @@ dependencies {
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.activity.compose)
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
