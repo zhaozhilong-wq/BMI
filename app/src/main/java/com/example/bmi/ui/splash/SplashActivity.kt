8c0853406e3ecf729e06ce4dbd79e8b6f73f4004
ZJSS <zjss@zjsdeMacBook-Pro.local>
2026-09-04 16:11:13 +0800

    在compose中设置导航栏边距

--- app/src/main/java/com/example/bmi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/bmi/ui/splash/SplashActivity.kt
@@ -5,36 +5,34 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.animation.PathInterpolator
-import android.animation.AnimatorListenerAdapter
+import android.graphics.Color
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.core.graphics.Insets
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.doOnLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.example.bmi.databinding.ActivitySplashBinding
-import com.example.bmi.ui.BaseActivity
 import com.example.bmi.ui.input.InputActivity
 import com.example.bmi.ui.main.MainActivity
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SplashActivity : BaseActivity<ActivitySplashBinding>() {
-    override fun createBinding(): ActivitySplashBinding {
-        return ActivitySplashBinding.inflate(layoutInflater)
-    }
+class SplashActivity : AppCompatActivity() {
 
     private val viewModel: SplashViewModel by viewModel()
 
 
-    override fun getInsets(insets: WindowInsetsCompat): Insets {
-        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        return Insets.of(systemBars.left, systemBars.top, systemBars.right, 0)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             SplashScreen(
                 onAnimationEnd = {
--- app/src/main/java/com/example/bmi/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/example/bmi/ui/splash/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
@@ -110,6 +111,7 @@ fun SplashScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF3659CF))
+            .navigationBarsPadding()
     ) {
 
         Box(
