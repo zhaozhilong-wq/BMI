1dd0abf5dbce091a4d3d227289b51420fd1bd773
ZJSS <zjss@zjsdeMacBook-Pro.local>
2026-09-07 11:39:11 +0800

    反馈页重构

--- app/src/main/java/com/example/bmi/ui/setting/FeedbackActivity.kt
+++ app/src/main/java/com/example/bmi/ui/setting/FeedbackActivity.kt
@@ -3,6 +3,8 @@ package com.example.bmi.ui.setting
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,62 +12,27 @@ import androidx.core.view.updateLayoutParams
 import com.example.bmi.databinding.ActivityFeedbackBinding
 import com.example.bmi.ui.BaseActivity
 
-class FeedbackActivity : BaseActivity<ActivityFeedbackBinding>() {
-    override fun createBinding() = ActivityFeedbackBinding.inflate(layoutInflater)
+class FeedbackActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
 
-            val systemBars = insets.getInsets(
-                WindowInsetsCompat.Type.systemBars()
-            )
-
-            val ime = insets.getInsets(
-                WindowInsetsCompat.Type.ime()
-            )
-
-            // 键盘高度
-            val imeBottom = ime.bottom
-
-            // 正常情况下使用导航栏高度
-            // 键盘弹出后使用键盘高度
-            val bottomInset = maxOf(
-                systemBars.bottom,
-                imeBottom
-            )
-
-            binding.inputText.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                bottomMargin = bottomInset + dpToPx(20)
-            }
-
-            binding.root.setPadding(
-                systemBars.left,
-                systemBars.top,
-                systemBars.right,
-                0
-            )
-
-            insets
-        }
-        binding.back.setOnClickListener {
-            finish()
-        }
-        binding.saveButton.setOnClickListener {
-            setResult(
-                Activity.RESULT_OK,
-                Intent().apply {
-                    putExtra("save_success", true)
+        setContent {
+            FeedbackScreen(
+                onBackClick = {
+                    finish()
+                },
+                onSaveClick = {
+                    setResult(
+                        Activity.RESULT_OK,
+                        Intent().apply {
+                            putExtra("save_success", true)
+                        }
+                    )
+                    finish()
                 }
             )
-            finish()
         }
     }
-
-    private fun dpToPx(dp: Int): Int {
-        return (dp * resources.displayMetrics.density).toInt()
-    }
-
-
 }
---
+++ app/src/main/java/com/example/bmi/ui/setting/FeedbackScreen.kt
@@ -0,0 +1,177 @@
+package com.example.bmi.ui.setting
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.example.bmi.R
+
+@Preview
+@Composable
+fun FeedbackScreenPreview()
+{
+    FeedbackScreen(
+        onBackClick = {},
+        onSaveClick = {}
+    )
+}
+
+@Composable
+fun FeedbackScreen(onBackClick: () -> Unit,
+                   onSaveClick: () -> Unit)
+{
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEAEAEE))
+            .statusBarsPadding()
+            .navigationBarsPadding()
+    ) {
+        HeaderFeedBack(onBackClick = onBackClick)
+
+        InputArea(onSaveClick = onSaveClick)
+    }
+}
+
+@Composable
+fun HeaderFeedBack(onBackClick: () -> Unit)
+{
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 18.5.dp, start = 15.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.arrow_left),
+            contentDescription = "Back",
+            modifier = Modifier
+                .size(24.dp)
+                .clickable{
+                    onBackClick()
+                }
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Text(
+            text = stringResource(R.string.feedback),
+            fontSize = 20.sp,
+            color = Color(0xFF000000),
+            fontFamily = FontFamily(Font(R.font.montserrat_extrabold)),
+            letterSpacing = (-0.01).em
+        )
+    }
+}
+
+@Composable
+fun InputArea(onSaveClick: () -> Unit)
+{
+    var text by remember {
+        mutableStateOf("")
+    }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(
+            start = 15.dp,
+            end = 15.dp,
+            top = 64.5.dp,
+            bottom = 40.dp
+        )
+        .imePadding()
+        .background(Color(0xFFFFFFFF), shape = androidx.compose.foundation.shape.RoundedCornerShape(12.dp))
+    ){
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .imePadding(),
+            placeholder = { Text(text = stringResource(R.string.feedback_or_suggestion),
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                color = Color.Black,
+                letterSpacing = (-0.01).em) },
+            textStyle = androidx.compose.ui.text.TextStyle(
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                color = Color.Black,
+                letterSpacing = (-0.01).em
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                errorContainerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            )
+            )
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(
+                    start = 30.dp,
+                    end = 30.dp,
+                    bottom = 10.dp
+                )
+                .height(55.dp)
+                .background(
+                    color = Color(0xFF3659CF),
+                    shape = RoundedCornerShape(27.5.dp)
+                )
+                .clickable {
+                    onSaveClick()
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.save),
+                fontSize = 20.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.montserrat_extrabold)
+                ),
+                color = Color.White,
+                letterSpacing = (-0.01).em
+            )
+        }
+
+    }
+}
